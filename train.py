5fe356fbb8c2a60b07747acf8125f4609d95d3bf
Emanuelle <emanuellenmoura@gmail.com>
2026-05-05 18:57:31 -0300

    modelo principal xgboost

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import QuantileTransformer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from utils.pre_processing import *
@@ -16,21 +17,27 @@ pd.set_option('display.float_format', lambda x: f'{x:.2f}')
 print(data.df.describe().T)
 #sinais de outliers nas colunas:
 colunas_outliers = ['TVOC[ppb]', 'eCO2[ppm]', 'PM1.0', 'PM2.5', 'NC0.5', 'NC1.0', 'NC2.5']
-data.box_plot_multi(colunas_outliers, "Distribuição em colunas com sinal de outlier")
-
+data.box_plot_multi(colunas_outliers, "Distribuição de Outliers")
 data.apply_log(colunas_outliers) #faz sentido para cauda muito longa, muitos outliers e regressão logística como baseline
-"""
-como é um problema de classificação binária,incêndio ou não incêndio, 
-regressão logística usada como baseline para provar se um algoritmo mais complexo vale a pena
-random forest escolhido pelas variáveis em escalas heterogêneas e 
-multicolinearidade presente entre os sensores, bom para mapear picos não-lineares de fumaça e 
-calcula a importância das variáveis -> permite identificar sensores redundantes
-"""
+data.box_plot_multi(colunas_outliers, "Em Escala Logarítmica")
+
 data.separar_base('Fire Alarm', columns=['Fire Alarm', 'UTC', 'CNT']) #removidas coluna de contagem de tempo e amostras pq modelo estava usando de gabarito
 verificar_base(data.X_train, data.X_test, data.y_train, data.y_test, 'Fire Alarm')
 
+data.heatmap() #7 sensores PM e NC trazem basicamente a mesma informação
+print(f"Análise de Multicolinearidade\n: {data.get_vif()}\n")
+
+"""
+regressão logística como baseline para testar se a natureza do problema é linear
+sofre com a alta multicolinearidade presente na base
+
+xgboost como modelo principal para mapear picos não-lineares na detecção de fumaça,
+lida nativamente com escalas heterogêneas, anula o impacto da multicolinearidade e fornece a 
+importância das variáveis, usado para isolar apenas os sensores úteis na versão final
+"""
 log_reg = Pipeline([('smote', SMOTE(random_state=42)),
-                    ('scaler', StandardScaler()),
+                    #forçar distribuição linear por causa dos outliers
+                    ('transformer', QuantileTransformer(output_distribution='normal', random_state=42)),
                     ('model', LogisticRegression(max_iter=1000))])
 
 log_reg.fit(data.X_train, data.y_train)
@@ -47,27 +54,30 @@ previsoes = log_reg.predict(data.X_test)
 print(f"\nAcurácia da regressão logistica: {accuracy_score(data.y_test, previsoes) * 100:.2f}%\n")
 print(f"Relatório de Classificação:\n{classification_report(data.y_test, previsoes)}")
 
-random_forest = Pipeline([('smote', SMOTE(random_state=42)),
-                          ('model', RandomForestClassifier(random_state=42))])
+#restrições específicas nos nomes das features do xgboost
+data.X_train.columns = data.X_train.columns.str.replace(r'[\[\]]', '', regex=True)
+data.X_test.columns = data.X_test.columns.str.replace(r'[\[\]]', '', regex=True)
 
-random_forest.fit(data.X_train, data.y_train)
-pred_treino = random_forest.predict(data.X_train)
-print(f"\nAcurácia de random forest nos dados de treino: {accuracy_score(data.y_train, pred_treino) * 100:.2f}%\n")
+xgb = Pipeline([('smote', SMOTE(random_state=42)),
+                ('model', XGBClassifier(random_state=0))])
+
+xgb.fit(data.X_train, data.y_train)
+pred_treino = xgb.predict(data.X_train)
+print(f"\nAcurácia de xgboost nos dados de treino: {accuracy_score(data.y_train, pred_treino) * 100:.2f}%\n")
 print(classification_report(data.y_train, pred_treino))
 
-y_pred = random_forest.predict(data.X_test)
-print(f"Acurácia de random forest: {accuracy_score(data.y_test, y_pred) * 100:.2f}%\n")
+y_pred = xgb.predict(data.X_test)
+print(f"Acurácia de xgboost: {accuracy_score(data.y_test, y_pred) * 100:.2f}%\n")
 print(f"Relatório de Classificação:\n{classification_report(data.y_test, y_pred)}")
 
 
-param_grid = {'model__n_estimators': [50, 100, 200],'model__max_depth': [5,  10, 20],
-              'model__min_samples_split': [2, 5, 10], 'model__min_samples_leaf': [1, 2, 4],
-              'model__max_features': ['sqrt', 'log2', None]}
+param_grid = {'model__learning_rate': [0.01, 0.05, 0.1, 0.2], 'model__max_depth': [3, 4, 5, 6],
+              'model__n_estimators': [50, 100, 200, 300], 'model__subsample': [0.8, 0.9, 1.0], 'model__colsample_bytree': [0.8, 0.9, 1.0]}
 
-random_forest_base = Pipeline([('smote', SMOTE(random_state=42)),
-                               ('model', RandomForestClassifier(random_state=42))])
+xgb_base = Pipeline([('smote', SMOTE(random_state=42)),
+                     ('model', XGBClassifier(random_state=0))])
 #50 combinações aleatórias, 5 folds de validação cruzada
-random_search = RandomizedSearchCV(estimator=random_forest_base, param_distributions=param_grid,
+random_search = RandomizedSearchCV(estimator=xgb_base, param_distributions=param_grid,
                                    n_iter=50, cv=5, verbose=2, random_state=42, n_jobs=-1)
 
 random_search.fit(data.X_train, data.y_train)
@@ -78,21 +88,19 @@ y_pred_rs = melhor_modelo.predict(data.X_test)
 print(f"Acurácia do melhor modelo nos dados de teste: {accuracy_score(data.y_test, y_pred_rs) * 100:.2f}%\n")
 print(f"Relatório de Classificação:\n{classification_report(data.y_test, y_pred_rs)}")
 
-data.heatmap() #7 sensores PM e NC trazem basicamente a mesma informação
-print(f"Análise de Multicolinearidade\n: {data.get_vif()}\n")
-colunas_top3 = data.X_train.columns[random_forest.named_steps['model'].feature_importances_.argsort()[-3:]]
+colunas_top3 = data.X_train.columns[xgb.named_steps['model'].feature_importances_.argsort()[-3:]]
 
 print(f"Sensores mantidos no modelo reduzido: {list(colunas_top3)}\n")
 X_train_reduzido = data.X_train[colunas_top3]
 X_test_reduzido = data.X_test[colunas_top3]
 
-random_forest_reduzido = Pipeline([('smote', SMOTE(random_state=0)),
-                                   ('model', RandomForestClassifier(n_estimators=50,max_depth=5,random_state=42))])
-cv_rf_reduzido = cross_val_score(random_forest_reduzido, X_train_reduzido, data.y_train, cv=5)
+xgb_reduzido = Pipeline([('smote', SMOTE(random_state=0)),
+                         ('model', XGBClassifier(random_state=0))])
+cv_rf_reduzido = cross_val_score(xgb_reduzido, X_train_reduzido, data.y_train, cv=5)
 print(f"Scores individuais por Fold: {np.round(cv_rf_reduzido * 100, 2)}%")
 print(f"Média dos 5 folds: {cv_rf_reduzido.mean() * 100:.2f}%\n")
 
-random_forest_reduzido.fit(X_train_reduzido, data.y_train)
-y_pred_reduzido = random_forest_reduzido.predict(X_test_reduzido)
+xgb_reduzido.fit(X_train_reduzido, data.y_train)
+y_pred_reduzido = xgb_reduzido.predict(X_test_reduzido)
 print(f"Acurácia do modelo reduzido: {accuracy_score(data.y_test, y_pred_reduzido) * 100:.2f}%\n")
 print(f"Relatório de Classificação do modelo reduzido:\n{classification_report(data.y_test, y_pred_reduzido)}")
